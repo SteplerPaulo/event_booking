96274d937b4dffb8487aadbada3ef5ee5d6d8ed8
Paulo Biscocho <paulobiscocho@ubx.ph>
2023-04-29 15:30:57 +0800

    Profile Screen

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -10,15 +10,189 @@ class ProfileView extends GetView<ProfileController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ProfileView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'ProfileView is working',
-          style: TextStyle(fontSize: 20),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_back),
         ),
+        title: const Text('Profile'),
       ),
+      body: ListView(
+        shrinkWrap: true,
+        padding: const EdgeInsets.fromLTRB(24, 12, 24, 12),
+        children: [
+          mainProfile(),
+          aboutMe(),
+          const SizedBox(height: 18),
+          interest(),
+        ],
+      ),
+    );
+  }
+
+  Widget mainProfile() {
+    return Container(
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const CircleAvatar(
+            radius: 60,
+            backgroundImage: AssetImage("assets/profile.jpg"),
+          ),
+          const SizedBox(height: 12),
+          const Text(
+            "Jennie Kim",
+            style: TextStyle(
+              color: Color(0xff110c26),
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(height: 24),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: const [
+                  Text(
+                    "350",
+                    style: TextStyle(
+                      color: Color(0xff110c26),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Following",
+                    style: TextStyle(
+                      color: Color(0xff747688),
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+              const VerticalDivider(
+                width: 50,
+                endIndent: 0,
+                indent: 0,
+                color: Colors.black,
+                thickness: 1,
+              ),
+              Column(
+                children: const [
+                  Text(
+                    "346",
+                    style: TextStyle(
+                      color: Color(0xff110c26),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Followers",
+                    style: TextStyle(
+                      color: Color(0xff747688),
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 24),
+          OutlinedButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.border_color),
+            label: const Text('Edit Profile'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget aboutMe() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        Text(
+          'About Me',
+          style: TextStyle(
+            color: Color(0xff110c26),
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        SizedBox(height: 24),
+        Text(
+          "Enjoy your favorite dishes and a lovely your friends and family and have a great time. Food from local food trucks will be available for purchase. Read More",
+        ),
+      ],
+    );
+  }
+
+  Widget interest() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "Interest",
+              style: TextStyle(
+                color: Color(0xff172b4d),
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const Spacer(),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.border_color,
+                size: 18,
+              ),
+              label: const Text('Change'),
+            )
+          ],
+        ),
+        Wrap(
+          spacing: 8,
+          children: const [
+            Chip(
+              label: Text("Games Online"),
+              backgroundColor: Colors.blue,
+            ),
+            Chip(
+              label: Text("Concert"),
+              backgroundColor: Colors.red,
+            ),
+            Chip(
+              label: Text("Music"),
+              backgroundColor: Colors.orange,
+            ),
+            Chip(
+              label: Text("Art"),
+              backgroundColor: Colors.purple,
+            ),
+            Chip(
+              label: Text("Movie"),
+              backgroundColor: Colors.green,
+            ),
+            Chip(
+              label: Text("Others"),
+              backgroundColor: Colors.cyan,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
